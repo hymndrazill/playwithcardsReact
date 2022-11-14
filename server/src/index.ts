e862c8e5e73edae9734bce28f8260467b1c498c6
hymndrazill <srihafiras@yahoo.fr>
2022-11-14 23:56:07 +0100

    added dot env + path so it can read it

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config({path:__dirname+'/.env'});
+
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Deck from './models/Deck';
+
 const app = express();
+
 app.use(express.json());
 
 const port = 5000;
@@ -14,9 +19,8 @@ app.post('/decks', async (req: Request, res: Response) => {
 });
 
  mongoose.connect(
-    'mongodb+srv://push3:push3@cluster0.yoh5c.mongodb.net/?retryWrites=true&w=majority'
-  ).then(()=>{
-    console.log("listening on port "+port);
+    process.env.MONGO_URL!).then(()=>{
+    console.log(`listening on port ${port}`);
     app.listen(port);
 
   })
