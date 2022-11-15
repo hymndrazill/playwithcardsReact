d4d745966790a7d47cea109938c499d5d98600f6
hymndrazill <srihafiras@yahoo.fr>
2022-11-16 00:03:25 +0100

    added Nav component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,7 @@ import {
   Route,
 } from "react-router-dom";
 import Deck from './Deck';
+import Nav from './Nav';
 
 const router = createBrowserRouter([
   {
@@ -23,7 +24,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <div className="container">
+    <Nav/>
     <RouterProvider router={router}/>
+    </div>
 
   </React.StrictMode>
 )
