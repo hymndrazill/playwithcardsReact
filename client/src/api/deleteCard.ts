7f0c62efecf8ff06df22998f204b98dfe400a97a
hymndrazill <srihafiras@yahoo.fr>
2022-11-15 18:16:19 +0100

    added methods

---
+++ client/src/api/deleteCard.ts
@@ -0,0 +1,8 @@
+import { TDeck } from './getDecks';
+import { API_URL } from './config';
+export async function deleteCard(deckId: string, index: number): Promise<TDeck>{
+   const res= await fetch(`${API_URL}/decks/${deckId}/cards/${index}`,{
+        method: "DELETE",
+      })
+      return res.json();
+}
---
+++ client/src/api/getDeck.ts
@@ -0,0 +1,9 @@
+import { TDeck } from './getDecks';
+import { API_URL } from './config';
+
+
+
+export async function getDeck(deckId: string){
+       const response = await fetch(`${API_URL}/decks/${deckId}`);
+       return response.json();
+      }
