a16e48c7e8ad945eba09728035a09a090e2ffb41
hymndrazill <srihafiras@yahoo.fr>
2022-11-15 18:16:48 +0100

    added endpoints after nesting the cards

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,9 @@ import cors from "cors";
 import { getDecks } from './controllers/getDeckController';
 import { createDecks } from './controllers/createDeckController';
 import { deleteDeck } from './controllers/deleteDeckController';
+import { createCardForDeck } from './controllers/createCardForDeckController';
+import { getDeck } from './controllers/getDeck';
+import { deleteCard } from './controllers/deleteCard';
 const app = express();
 app.use(cors({
     origin: "*"
@@ -17,13 +20,14 @@ app.use(express.json());
 const port = 5000;
 
 app.get('/decks',getDecks)
-
-
 app.post('/decks', createDecks);
 app.delete('/decks/:deckId',deleteDeck);
+app.get("/decks/:deckId", getDeck)
+app.post("/decks/:deckId/cards", createCardForDeck)
+app.delete("/decks/:deckId/cards/:index", deleteCard)
 
 
- mongoose.connect(
+mongoose.connect(
     process.env.MONGO_URL!).then(()=>{
     console.log(`listening on port ${port}`);
     app.listen(port);
