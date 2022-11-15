02553a19630244e2a30b0dc47748180ce4146f22
hymndrazill <srihafiras@yahoo.fr>
2022-11-15 01:06:42 +0100

    added the fetch and post methods + styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,60 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+type TDeck = {
+  title: string,
+  _id:string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('');
+  const [decks,setDecks]= useState<TDeck[]>([]);
+
+  const handleSubmit = async (e:React.FormEvent) => {
+    e.preventDefault();
+   await fetch("http://localhost:5000/decks", {
+     
+      method: "POST",
+      body: JSON.stringify({
+        title
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    setTitle("");
+  }
+
+
+  useEffect(()=>{
+    async function fechDecks(){
+      const response = await fetch("http://localhost:5000/decks");
+      const newDecks = await response.json();
+      setDecks(newDecks);
+    }
+  
+    fechDecks();
+    return () => {
+      console.log("were are done clean up");
+    }
+  },[]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="decks">
+        {decks?.map((deck)=>(
+          <li key={deck._id}>{deck.title}</li>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <form onSubmit={handleSubmit}>
+        <label htmlFor='title'>Deck Title</label>
+        <input id="title" type="text" placeholder='Deck Title'
+        value={title}
+        onChange={(e: React.ChangeEvent <HTMLInputElement> )=> setTitle(e.target.value)}
+        />
+        <button>Create Deck</button>
+     </form>
     </div>
   )
 }
