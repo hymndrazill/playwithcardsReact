545d6a4c231b29bd68124f37b72962dcfabf0c55
hymndrazill <srihafiras@yahoo.fr>
2022-11-15 01:06:58 +0100

    added the get endpoint + cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,21 @@ dotenv.config({path:__dirname+'/.env'});
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Deck from './models/Deck';
-
+import cors from "cors";
 const app = express();
-
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 const port = 5000;
+
+app.get('/decks',async (req:Request,res:Response)=>{
+    const decks = await Deck.find();
+    res.status(200).json(decks);
+})
+
+
 app.post('/decks', async (req: Request, res: Response) => {
     const newDeck = new Deck({
         title: req.body.title,
